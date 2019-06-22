f8adf6f589ddc1410f7ce5bfb1fdddd3b26e0c90
Christopher Harrison <cnharrison@gmail.com>
2019-06-21 22:11:53 -0400

    repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
-const enhancer = require('./enhancer.js');
+const { repair } = require("./enhancer.js");
 // test away!
+
+const item = {
+  name: "the mf test item",
+  durability: 75,
+  enhancement: 30
+};
+
+console.log(repair(item));
+
+describe("repair function", () => {
+  it("sets durability to 100", () => {
+    const result = repair(item);
+    expect(result.durability).toBe(100);
+  });
+});
